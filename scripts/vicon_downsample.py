7fc70f636ee93573f615c46c0e97c407fc72d3ff
Benjamin-Riedi <briedi@ethz.ch>
2026-07-17 14:13:23 +0200

    fix rate to 100hz

--- scripts/vicon_downsample.py
+++ scripts/vicon_downsample.py
@@ -2,6 +2,7 @@ import rospy
 import numpy as np
 import tf2_ros
 
+from threading import Thread
 from control_utils.msg import ScalarStamped
 from geometry_msgs.msg import TransformStamped
 from python_utils.gen import quaternion_to_normal_vector, angles_from_normal_vector
@@ -21,8 +22,14 @@ class ViconAnglePublisher:
         self.bottom_pub = rospy.Publisher(self.bottom_angle_topic, ScalarStamped, queue_size=1)
         self.sub_topic = rospy.get_param('/vicon/pendulum')
 
+        self.top_angle = ScalarStamped()
+        self.bottom_angle = ScalarStamped()
+
         self.downsample_factor = rospy.get_param('/vicon/downsample_factor', 10)
         self.hold = 0
+        self.r = rospy.Rate(100)
+
+        rospy.Subscriber(self.sub_topic, TransformStamped, self.callback)
     
     def init_variables(self):
         self.x = 0.0
@@ -36,39 +43,40 @@ class ViconAnglePublisher:
         self.angle_threshold = 0.01
 
     def callback(self, tf_msg):
-        if self.hold <= self.downsample_factor:
-            self.hold += 1
-            return
-        else:
-            self.hold = 0
+        # if self.hold <= self.downsample_factor:
+        #     self.hold += 1
+        #     return
+        # else:
+        #     self.hold = 0
 
-            self.top_angle = ScalarStamped()
-            self.bottom_angle = ScalarStamped()
 
-            try:
-                transform = self.tf_buffer.lookup_transform('vicon/world', 'vicon/benjamin_v2/Root',  rospy.Time(0))
-            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-                rospy.logerr("Failed to lookup transform")
+        try:
+            transform = self.tf_buffer.lookup_transform('vicon/world', 'vicon/benjamin_v2/Root',  rospy.Time(0))
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.logerr("Failed to lookup transform")
 
-            # translation = transform.transform.translation
-            # self.valid = abs(translation.x - self.x) < self.pos_threshold and abs(translation.y - self.y) < self.pos_threshold and abs(translation.z - self.z) < self.pos_threshold
-            rotation = transform.transform.rotation
-            quat = np.array([[rotation.x, rotation.y, rotation.z, rotation.w]])
-            normal_vector = quaternion_to_normal_vector(quat)
-            # self.angle_y, self.angle_x = angles_from_normal_vector(-normal_vector)
-            self.top_angle.scalar, self.bottom_angle.scalar = angles_from_normal_vector(-normal_vector)
-            self.top_angle.scalar = -self.top_angle.scalar  # flip sign for top angle to match the convention used in the model
-            self.top_angle.header.stamp = rospy.Time.now()
-            self.bottom_angle.header.stamp = rospy.Time.now()
-            
+        rotation = transform.transform.rotation
+        quat = np.array([[rotation.x, rotation.y, rotation.z, rotation.w]])
+        normal_vector = quaternion_to_normal_vector(quat)
+        # self.angle_y, self.angle_x = angles_from_normal_vector(-normal_vector)
+        self.top_angle.scalar, self.bottom_angle.scalar = angles_from_normal_vector(-normal_vector)
+        # self.bottom_angle.scalar, self.top_angle.scalar = angles_from_normal_vector(-normal_vector)
+        self.bottom_angle.scalar = -self.bottom_angle.scalar  # flip sign for top angle to match the convention used in the model
+
+        self.top_angle.header.stamp = rospy.Time.now()
+        self.bottom_angle.header.stamp = rospy.Time.now()
+
+    def publisher(self):
+        while not rospy.is_shutdown():
             self.top_pub.publish(self.top_angle)
             self.bottom_pub.publish(self.bottom_angle)
+            self.r.sleep()
 
     def run(self):
-        rospy.Subscriber(self.sub_topic, TransformStamped, self.callback)
         rospy.spin()
 
 
 if __name__ == "__main__":
     node = ViconAnglePublisher()
+    Thread(target=node.publisher, daemon=True).start()
     node.run()
